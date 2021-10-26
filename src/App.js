a1c78e1192d96a4bd8ba6bc093a43b876cb2f252
1TaylanOzturk <taylanozturk.freedom@gmail.com>
2021-10-26 22:27:01 +0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,68 @@
+import "./styles/App.css";
+import Layout from "./components/Layout";
+import { ADAGES, imgSources } from "./source";
+import Carousel from "nuka-carousel";
+import Audio from "./audios/audio.mp3";
+import ReactAudioPlayer from "react-audio-player";
+
+const shuffle = (array) => {
+  let currentIndex = array.length,
+    randomIndex;
+
+  while (currentIndex !== 0) {
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+
+    [array[currentIndex], array[randomIndex]] = [
+      array[randomIndex],
+      array[currentIndex],
+    ];
+  }
+
+  return array;
+};
+
+function App() {
+  let adages = ADAGES;
+  adages = shuffle(adages);
+
+  return (
+    <Layout>
+      <div className="title mb-5" style={{ height: window.innerHeight }}>
+        29 Ekim
+      </div>
+      <div>
+        <Carousel className="carousel mb-5" autoplayInterval={3000} autoplay>
+          {imgSources.map((url, index) => {
+            return (
+              <div key={index}>
+                <img src={url} alt="IMG" className="image" />
+              </div>
+            );
+          })}
+        </Carousel>
+      </div>
+
+      <div className="mb-5">
+        {adages.map((element, index) => {
+          return (
+            <div className="card-adage" key={index}>
+              {element[0]}
+            </div>
+          );
+        })}
+      </div>
+
+      <div>
+        <ReactAudioPlayer
+          className="audio-player mb-3"
+          src={Audio}
+          volume={0.2}
+          controls
+        />
+      </div>
+    </Layout>
+  );
+}
+
+export default App;
---
+++ src/source.js
@@ -0,0 +1,58 @@
+export const ADAGES = [
+  [
+    "Benim naçiz vücudum elbet bir gün toprak olacaktır, ancak Türkiye Cumhuriyeti ilelebet payidar kalacaktır.",
+  ],
+  [
+    "Cumhuriyet, fikir serbestliği taraftarıdır. Samimi ve meşru olmak şartıyla her fikre hürmet ederiz.",
+  ],
+  [
+    "Ey yükselen yeni nesil! İstikbal sizsiniz. Cumhuriyeti biz kurduk, onu yükseltecek ve yaşatacak sizsiniz",
+  ],
+  [
+    "Türk milletinin karakterine ve adetlerine en uygun olan idare, Cumhuriyet idaresidir.",
+  ],
+  ["Efendiler! Yarın cumhuriyeti ilan edeceğiz", "5.mp3"],
+  [
+    "Cumhuriyet, yeni ve sağlam esaslariyle, Türk milletini emin ve sağlam bir istikbal yoluna koyduğu kadar, asıl fikirlerde ve ruhlarda yarattığı güvenlik itibariyle, büsbütün yeni bir hayatın müjdecisi olmuştur.",
+  ],
+  [
+    "Cumhuriyetimiz öyle zannolunduğu gibi zayıf değildir. Cumhuriyet bedava da kazanılmış değildir. Bunu elde etmek için kan döktük. Her tarafta kırmızı kanımızı akıttık. İcabında müesseselerimizi müdafaa için lâzım olanı yapmaya hazırız.",
+  ],
+  [
+    "Hürriyet olmayan bir memlekette ölüm ve çöküş vardır. Her ilerleyişin ve kurtuluşun anası hürriyettir.",
+  ],
+  [
+    "Bir ulus, sımsıkı birbirine bağlı olmayı bildikçe yer yüzünde onu dağıtabilecek bir güç düşünülemez.",
+  ],
+  [
+    "Şayet ölecek olursam, memlekete ait söyleyecek hiçbir şeyim yoktur. Çünkü yürürlükteki Cumhuriyet yasaları bu işleri temine yeterlidir.",
+  ],
+  [
+    "Cumhuriyet düşüncede, bilgide, sağlıkta güçlü ve yüksek karakterli koruyucular ister.",
+  ],
+  [
+    "Dünya üzerinde yaşamış ve yaşayan milletler arasında demokrat doğan yegâne millet Türklerdir.",
+  ],
+  [
+    "Bütün dünya bilsin ki benim için bir yandaşlık vardır: Cumhuriyet yandaşlığı, düşünsel ve toplumsal devrim yandaşlığı. Bu noktada yeni Türkiye topluluğunda bir bireyi bunun dışında düşünmek istemiyorum.",
+  ],
+  [
+    "Cumhuriyet, demokratik idarenin tam ve mükemmel bir ifadesidir. Bu rejim, halkın gelişimini ve yükselişini sağlayan, onlardan esirlik, soysuzluk, dalkavukluk hislerini uzaklaştıran bir yoldur.",
+  ],
+  [
+    "Yetişecek çocuklarımıza ve gençlerimize, görecekleri tahsilin hududu ne olursa olsun, en evvel, her şeyden evvel Türkiye’nin istikbaline, kendi benliğine, millî geleneklerine düşman olan bütün unsurlarla mücadele etmek lüzumu öğretilmelidir.",
+  ],
+];
+
+export const imgSources = [
+  "https://upload.wikimedia.org/wikipedia/commons/4/49/Turkish_officers_after_the_War_of_Independence.jpg",
+  "https://upload.wikimedia.org/wikipedia/commons/4/48/Turkish_infantryman_during_the_War_of_Independence.jpg",
+  "https://www.researchgate.net/profile/Goekcen-Uzunkoeprue/publication/339566942/figure/fig1/AS:863564571086857@1582901246448/The-Turkish-War-of-Independence.jpg",
+  "https://idsb.tmgrup.com.tr/2016/04/07/GenelBuyuk/the-picture-depicts-the-turkish-war-of-independenc-1460062421423_rs.jpg?v=1460101316",
+  "https://i1.trekearth.com/photos/59441/img_2658.jpg",
+  "https://pbs.twimg.com/media/DIfp-kbXYAA1vjf?format=jpg&name=900x900",
+  "https://upload.wikimedia.org/wikipedia/commons/4/48/Turkish_infantryman_during_the_War_of_Independence.jpg",
+  "https://pbs.twimg.com/media/EDMj0Z8UYAEwL0k?format=jpg&name=900x900",
+  "https://upload.wikimedia.org/wikipedia/commons/thumb/d/da/Ermoscharge.JPG/345px-Ermoscharge.JPG",
+  
+];
